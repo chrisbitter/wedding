933d2586bf05aacf2c71cef3295fe711b889d90b
cs671660 <christian.scheiderer@rwth-aachen.de>
2019-11-22 09:45:05 +0100

    Save from IMA laptop

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
-from flask import Flask, render_template, flash, url_for, redirect, request
-from flask_login import LoginManager, login_required, current_user, login_user, logout_user
+from flask import Flask, render_template, url_for, redirect, request, jsonify
+from flask_login import LoginManager, login_required, \
+    current_user as current_group, login_user, logout_user
 from sqlalchemy.exc import IntegrityError
 from forms import LoginForm
 from http import HTTPStatus
@@ -8,110 +9,252 @@ import logging
 from urllib.parse import urlparse, urljoin
 from config import Config
 import csv
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-#logger.setLevel(logging.DEBUG)
+import pandas as pd
 
 path_agenda = "resources/agenda.csv"
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from models import Group, User, db
+from models import Group, Guest, db
 
 db.init_app(app)
 
+debug = True
 
-try:
+if debug:
     with app.app_context():
-
         db.drop_all()
         db.create_all()
 
-        group_name = "Familie Nerz"
+        # only for debugging
+        group_id = 0
+        group_name = "Team Nerz"
         token = "nerz"
-
-        group = Group(name=group_name, token_hash=hash(token))
+        group = Group(id=group_id, name=group_name, token=token, admin=True)
         db.session.add(group)
-
-        group_id = Group.query.filter_by(name=group_name).first().id
-
-        nerz1 = User(group_id=group_id, name='Sir Nerz')
-        nerz2 = User(group_id=group_id, name='Nerzdame')
+        nerz1 = Guest(group_id=group_id, name='Kristina Bitter')
+        nerz2 = Guest(group_id=group_id, name='Christian Scheiderer')
         db.session.add(nerz1)
         db.session.add(nerz2)
         db.session.commit()
 
-        # users = User.query.filter_by(group_id=group_id).all()
+        group_id = 1
+        group_name = "Familie Dachs"
+        token = "dachs"
+        group = Group(id=group_id, name=group_name, token=token)
+        db.session.add(group)
+        dachs1 = Guest(group_id=group_id, name='Sir Dachs')
+        dachs2 = Guest(group_id=group_id, name='Mrs Dachs')
+        dachs3 = Guest(group_id=group_id, name='Frechdachs')
+        db.session.add(dachs1)
+        db.session.add(dachs2)
+        db.session.add(dachs3)
+        db.session.commit()
 
-except IntegrityError:
-    logger.warning("Database already initialized with admin account")
+        group_id = 2
+        group_name = "Fuchs"
+        token = "fuchs"
+        group = Group(id=group_id, name=group_name, token=token)
+        db.session.add(group)
+        fuchs = Guest(group_id=group_id, name='Mr. Fuchs')
+        db.session.add(fuchs)
+        db.session.commit()
+
 
 def get_users(group_id):
-    users = User.query.with_entities(User.id, User.name, User.rsvp,
-                                     User.food_choice).filter_by(
+    users = Guest.query.with_entities(Guest.id, Guest.name,
+                                      Guest.rsvp).filter_by(
         group_id=group_id).all()
 
-    users = [{"id": user[0], "name": user[1], "rsvp": user[2],
-              "food_choice": user[3]} for user in users]
+    users = [{"id": user[0], "name": user[1], "rsvp": user[2]} for user in
+             users]
 
     return users
 
-def get_agenda():
-    with open(path_agenda) as f:
 
-        agenda_items = [{"name": name, "time": time} for name, time in csv.reader(f, delimiter='\t')]
+def get_all_groups():
+    groups = Group.query.all()
+
+    result = []
+
+    for group in groups:
+
+        group_id = group.id
+        group_name = group.name
+
+        if group.is_admin():
+            group_token = None
+        else:
+            group_token = group.token
+
+        users = get_users(group_id)
+
+        group_data = {"id": group_id, "name": group_name, "token": group_token,
+                      "users": users}
+
+        result.append(group_data)
+
+    return result
+
+
+def get_agenda():
+    agenda = pd.read_csv(path_agenda)
+
+    agenda_items = agenda.to_dict(orient="records")
 
     return agenda_items
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
+
 @app.route('/agenda')
 @login_required
 def agenda():
-
     agenda_items = get_agenda()
 
-    return render_template('agenda.html', title='Agenda', active_page="agenda", agenda_items=agenda_items)
+    is_admin = current_group.is_admin()
+
+    return render_template('agenda.html', title='Agenda', active_page="agenda",
+                           agenda_items=agenda_items, is_admin=is_admin)
+
 
 @app.route('/')
-@app.route('/rsvp')
 @login_required
-def rsvp():
-    group = current_user
+def welcome():
+    group = current_group
 
     users = get_users(group.id)
 
-    print(users)
+    is_admin = current_group.is_admin()
 
-    return render_template('rsvp.html', title='RSVP', active_page="rsvp", group_name=group.name, users=users)
+    return render_template('welcome.html', title='031020',
+                           active_page="welcome", is_admin=is_admin)
+
+
+@app.route('/rsvp')
+@login_required
+def rsvp():
+    group = current_group
+
+    users = get_users(group.id)
+
+    is_admin = current_group.is_admin()
+
+    return render_template('rsvp.html', title='RSVP', active_page="rsvp",
+                           group_name=group.name, users=users,
+                           is_admin=is_admin)
+
 
 @app.route("/update_rsvp", methods=['POST'])
 @login_required
 def update_rsvp():
-
     guest_id = request.form["id"]
     choice = request.form["choice"]
 
-    user = User.query.filter_by(id=guest_id).first()
+    if choice == "":
+        choice = None
+    else:
+        choice = choice == "true"
 
-    user.rsvp = choice == "true"
+    user = Guest.query.filter_by(id=guest_id).first()
+
+    user.rsvp = choice
 
     db.session.commit()
 
+    app.logger.info(f"Update RSVP {user}")
+
     return "", HTTPStatus.NO_CONTENT
+
 
 @app.route('/location')
 @login_required
 def location():
-    return render_template('location.html', title='Location', active_page="location")
+    is_admin = current_group.is_admin()
+
+    return render_template('location.html', title='Location',
+                           active_page="location", is_admin=is_admin)
 
 
+@app.route('/overview')
+@login_required
+def overview():
+    if current_group.is_admin():
+        groups = get_all_groups()
+
+        is_admin = current_group.is_admin()
+
+        return render_template('overview.html', title='Overview',
+                               active_page="overview", groups=groups,
+                               is_admin=is_admin)
+
+
+@app.route('/create_group', methods=['POST'])
+@login_required
+def create_group():
+    if current_group.is_admin():
+
+        group_name = str(request.form["name"])
+        token = str(request.form["token"])
+        user_names = request.form.getlist("user_names[]")
+
+        user_names = [str(name) for name in user_names if name != ""]
+
+        if len(user_names) == 1:
+            group_name = user_names[0]
+
+        group = Group(name=group_name, token=token)
+        db.session.add(group)
+        db.session.commit()
+
+        app.logger.info(f"Create group {group}")
+
+        group_id = group.id
+
+        for user_name in user_names:
+            guest = Guest(group_id=group_id, name=user_name)
+            db.session.add(guest)
+
+            app.logger.info(f"Create guest {guest}")
+
+        db.session.commit()
+
+        users = get_users(group_id)
+
+        group_data = {"id": group_id, "name": group_name, "token": token,
+                      "users": users}
+
+        return jsonify(group_data)
+
+
+@app.route('/delete_group', methods=['POST'])
+@login_required
+def delete_group():
+    if current_group.is_admin():
+
+        group_id = request.form["id"]
+
+        groups = Group.query.filter_by(id=group_id).all()
+        guests = Guest.query.filter_by(group_id=group_id).all()
+
+        for group in groups:
+            app.logger.info(f"Delete {group}")
+            db.session.delete(group)
+
+        for guest in guests:
+            app.logger.info(f"Delete {guest}")
+            db.session.delete(guest)
+
+        db.session.commit()
+
+        return "", HTTPStatus.NO_CONTENT
+
+    else:
+        return "", HTTPStatus.UNAUTHORIZED
 
 
 @login_manager.user_loader
@@ -131,33 +274,31 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
+    if current_group.is_authenticated:
         return redirect(url_for('agenda'))
-    # Here we use a class of some kind to represent and validate our
-    # client-side form data. For example, WTForms is a library that will
-    # handle this for us, and we use a custom LoginForm to validate.
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        # Login and validate the user.
-        # user should be an instance of your `User` class
-        # login_user(user)
 
-        group = Group.query.filter_by(token_hash=hash(form.token.data)).first()
+        group = Group.query.filter_by(token=form.token.data).first()
 
-        if group is None: # or not user.check_password(form.password.data):
+        app.logger.info(f"Login {group}")
+
+        if group is None:  # or not user.check_password(form.password.data):
             return redirect(url_for('login'))
         else:
-            logger.debug("login successful")
+            app.logger.debug("login successful")
 
             next = flask.request.args.get('next')
             # is_safe_url should check if the url is safe for redirects.
             # See http://flask.pocoo.org/snippets/62/ for an example.
 
-            logger.debug(next)
-            logger.debug(is_safe_url(next))
+            app.logger.debug(next)
+            app.logger.debug(is_safe_url(next))
 
             if not is_safe_url(next):
                 return flask.abort(400)
@@ -168,7 +309,5 @@ def login():
     return flask.render_template('login.html', form=form, title='Login')
 
 
-
 if __name__ == '__main__':
-
     app.run(debug=True, use_reloader=False)
--- models.py
+++ models.py
@@ -5,11 +5,15 @@ db = SQLAlchemy()
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
-    token_hash = db.Column(db.String, unique=True)
+    token = db.Column(db.String, unique=True)
     admin = db.Column(db.Boolean, default=False)
 
+    def __repr__(self):
+        return f'Group <{self.name} | id {self.id} | Admin {self.admin}>'
+
+
     def is_active(self):
-        """True, as all users are active."""
+        """True, as all groups are active."""
         return True
 
     def get_id(self):
@@ -17,27 +21,27 @@ class Group(db.Model):
         return self.id
 
     def is_authenticated(self):
-        """Return True if the user is authenticated."""
+        """Return True if the group is authenticated."""
         return True
 
     def is_anonymous(self):
-        """False, as anonymous users aren't supported."""
+        """False, as anonymous groups aren't supported."""
         return False
 
     def is_admin(self):
         return self.admin
 
-class User(db.Model):
-    __tablename__ = 'user'
+class Guest(db.Model):
+    __tablename__ = 'guest'
 
     id = db.Column(db.Integer, primary_key=True)
     group_id = db.Column(db.Integer)
     name = db.Column(db.String)
     rsvp = db.Column(db.Boolean, nullable=True)
-    food_choice = db.Column(db.Integer, nullable=True)
+    # food_choice = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
-        return f'<{self.name} | Group {self.group_id} | RSVP {self.rsvp} | Food {self.food_choice}>'
+        return f'Guest <{self.name} | Group {self.group_id} | RSVP {self.rsvp}>'
 
     def check_password(self, password):
         return self.password_hash == str(hash(password))
